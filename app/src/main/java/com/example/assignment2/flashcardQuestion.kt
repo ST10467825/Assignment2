6b17b66beac1fc2fd5a4ecb474d3a2a2da569b32
Kgomotso Mbele <kgomotsombele@outlook.com>
2025-04-30 13:36:59 +0200

    Added flashcard page Added flashcard layout Added minor design to layout

---
+++ app/src/main/java/com/example/assignment2/flashcardQuestion.kt
@@ -0,0 +1,29 @@
+package za.co.varsitycollege.st10467825_assignment2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flashcard_question)
+
+        //Declarations
+        val questionView = findViewById< TextView>(R.id.questionTextView)
+        val answerEditText = findViewById<EditText>(R.id.answerEditText)
+        val feedbackTextView = findViewById<TextView>(R.id.feedbackTextView)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val scoreTextView = findViewById<TextView>(R.id.scoreTextView)
+
+
+    }
+}
